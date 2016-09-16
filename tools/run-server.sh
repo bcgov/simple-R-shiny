ede5285f676fc02a529fe8c77a9726d941052048
Chris Coldwell <chris.coldwell@gmail.com>
2016-09-16 16:32:18 -0700

    moving things around to make it easier to mount and copy

--- tools/run-server.sh
+++ tools/run-server.sh
@@ -4,4 +4,5 @@
 # mkdir -p /var/log/shiny-server
 # chown shiny.shiny /var/log/shiny-server
 
-exec shiny-server >> /var/log/shiny-server.log 2>&1
+# exec shiny-server >> /var/log/shiny-server/shiny-server.log 2>&1
+exec shiny-server --verbose
