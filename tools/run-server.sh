6be7941592b5fc46130299db4ee7a3c68cb6ff1c
Chris Coldwell <chris.coldwell@gmail.com>
2016-12-08 16:49:15 -0800

    add output

--- tools/run-server.sh
+++ tools/run-server.sh
@@ -4,6 +4,9 @@ directory=/etc/shiny-server
 configuration=shiny-server.conf
 temp=shiny-server.conf.tmp
 
+echo 'running as'
+echo $me
+
 sed -e "s/^run_as .*;$/run_as $me;/"  $directory/$configuration > $directory/$temp
 mv $directory/$temp $directory/$configuration
 
