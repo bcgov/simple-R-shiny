11171341746a62061d55e6802b741c8a5b7eccf8
Chris Coldwell <chris.coldwell@gmail.com>
2016-12-14 22:07:08 -0800

    comment out unnecessary code

--- tools/run-server.sh
+++ tools/run-server.sh
@@ -11,10 +11,10 @@ directory=/etc/shiny-server
 configuration=shiny-server.conf
 temp=shiny-server.conf.tmp
 
-echo 'running as'
-echo $me
+# echo 'running as'
+# echo $me
 
-sed -e "s/^run_as .*;$/run_as $me;/"  $directory/$configuration > $directory/$temp
-mv $directory/$temp $directory/$configuration
+# sed -e "s/^run_as .*;$/run_as $me;/"  $directory/$configuration > $directory/$temp
+# mv $directory/$temp $directory/$configuration
 
 shiny-server
